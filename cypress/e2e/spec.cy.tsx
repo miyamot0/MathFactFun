91e4253c79a0498679494aac51d5a9b32327bc52
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-09-27 14:58:09 -0500

    Start of e2e

--- cypress/e2e/spec.cy.tsx
+++ cypress/e2e/spec.cy.tsx
@@ -1,7 +1,8 @@
 import * as authUser from "../fixtures/auth-user.json";
 
-describe("After authenticating, should arrive at landing page", () => {
+describe("Basic Navigation, after authenticating", () => {
   let sideLinks: Cypress.Chainable<JQuery<HTMLElement>>;
+  const nSideElements = 5;
 
   beforeEach(() => {
     cy.restoreLocalStorage();
@@ -11,22 +12,81 @@ describe("After authenticating, should arrive at landing page", () => {
     cy.saveLocalStorage();
   });
 
-  it("Arrives at landing, authenticated", () => {
+  it("Should successfully log in", () => {
+    cy.viewport(1920, 1080)
+
     const { email, password } = authUser;
     cy.login(email, password);
-
     // Probe the landing page
     cy.visit("/login");
+
+  });
+
+  it("Should land on the welcome/landing page, once authenticated", () => {
+    cy.viewport(1920, 1080)
     cy.contains("Welcome");
+  });
+
+  it("Should display modal and dismiss as appropriate", () => {
+    cy.viewport(1920, 1080)
+
+    const navbarLinks = cy.get("div.navbar > ul > li");
+
+    navbarLinks.first().click();
+
+    cy.wait(100).get("div.navbar-modal").should('exist');
+
+    //const closeButton = cy.get("div.license-modal > button");
+
+    //closeButton.first().wait(500).click();
+
+    //
+    //sideLinks = cy.get("nav.links > ul > li");
+    //sideLinks.should("have.length", nSideElements);
+    //sideLinks.next().first().click();
+  });
+
+  /*
+
+  it("Arrives at information, authenticated", () => {
+    // Probe the information page
+    cy.wait(2000).contains("Information and Start-up Guide");
 
     sideLinks = cy.get("nav.links > ul > li");
-    sideLinks.should("have.length", 5);
-    // First link is home page
+    sideLinks.should("have.length", nSideElements);
+
+    sideLinks.next().next().first().click();
   });
 
-  it("Arrives at landing, authenticated", () => {
+  it("Arrives at Student Dashboard, authenticated", () => {
     // Probe the information page
-    sideLinks.next().first().click();
-    cy.contains("Information and Start-up Guide");
+    cy.wait(2000).contains("Student Dashboard");
+
+    sideLinks = cy.get("nav.links > ul > li");
+    sideLinks.should("have.length", nSideElements);
+
+    sideLinks.next().next().next().first().click();
   });
+
+  it("Arrives at Practice Dashboard, authenticated", () => {
+    // Probe the information page
+    cy.wait(2000).contains("Intervention Dashboard");
+
+    sideLinks = cy.get("nav.links > ul > li");
+    sideLinks.should("have.length", nSideElements);
+
+    sideLinks.next().next().next().next().first().click();
+  });
+
+  it("Arrives at Create Student Page, authenticated", () => {
+    // Probe the information page
+    cy.wait(2000).contains("Add a new student");
+
+    sideLinks = cy.get("nav.links > ul > li");
+    sideLinks.should("have.length", nSideElements);
+
+    sideLinks.next().next().next().next().first().click();
+  });
+
+  */
 });
